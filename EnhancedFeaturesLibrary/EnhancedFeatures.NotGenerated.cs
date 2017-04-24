99e55876d7b7dccc1156117bbf4fc538a789a82f
DJ Doena <karsten@yajirobo.de>
2017-04-24 19:10:29 +0200

    1.0.1.0

--- EnhancedFeaturesLibrary/EnhancedFeatures.NotGenerated.cs
+++ EnhancedFeaturesLibrary/EnhancedFeatures.NotGenerated.cs
@@ -1,4 +1,106 @@
-﻿// xsd.exe /c /l:cs /f EnhancedFeatures.xsd /n:DoenaSoft.DVDProfiler.EnhancedFeatures
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+// xsd.exe /c /l:cs /f EnhancedFeatures.xsd /n:DoenaSoft.DVDProfiler.EnhancedFeatures
 
 namespace DoenaSoft.DVDProfiler.EnhancedFeatures
-{ }
+{
+    public sealed partial class EnhancedFeaturesList
+    {
+        private static XmlSerializer s_XmlSerializer;
+
+        [XmlIgnore]
+        public static XmlSerializer XmlSerializer
+        {
+            get
+            {
+                if (s_XmlSerializer == null)
+                {
+                    s_XmlSerializer = new XmlSerializer(typeof(EnhancedFeaturesList));
+                }
+
+                return (s_XmlSerializer);
+            }
+        }
+
+        public static EnhancedFeaturesList Deserialize(String fileName)
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                EnhancedFeaturesList instance = (EnhancedFeaturesList)(XmlSerializer.Deserialize(fs));
+
+                return (instance);
+            }
+        }
+
+        public static void Serialize(EnhancedFeaturesList instance
+            , String fileName)
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.Read))
+            {
+                using (XmlTextWriter xtw = new XmlTextWriter(fs, Encoding.UTF8))
+                {
+                    xtw.Formatting = Formatting.Indented;
+
+                    XmlSerializer.Serialize(xtw, instance);
+                }
+            }
+        }
+
+        public void Serialize(String fileName)
+        {
+            Serialize(this, fileName);
+        }
+    }
+
+    public sealed partial class EnhancedFeatures
+    {
+        private static XmlSerializer s_XmlSerializer;
+
+        [XmlIgnore]
+        public static XmlSerializer XmlSerializer
+        {
+            get
+            {
+                if (s_XmlSerializer == null)
+                {
+                    s_XmlSerializer = new XmlSerializer(typeof(EnhancedFeatures));
+                }
+
+                return (s_XmlSerializer);
+            }
+        }
+
+        public static EnhancedFeatures Deserialize(String fileName)
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                EnhancedFeatures instance = (EnhancedFeatures)(XmlSerializer.Deserialize(fs));
+
+                return (instance);
+            }
+        }
+
+        public static void Serialize(EnhancedFeatures instance
+            , String fileName)
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.Read))
+            {
+                using (XmlTextWriter xtw = new XmlTextWriter(fs, Encoding.UTF8))
+                {
+                    xtw.Formatting = Formatting.Indented;
+
+                    XmlSerializer.Serialize(xtw, instance);
+                }
+            }
+        }
+
+        public void Serialize(String fileName)
+        {
+            Serialize(this, fileName);
+        }
+    }
+}
