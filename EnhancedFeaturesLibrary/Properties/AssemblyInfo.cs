a903f90a4c084865ac186d4b6a81018591f3a9d9
DJ Doena <github@doena-soft.de>
2022-09-24 12:16:19 +0200

    1.0.2

--- EnhancedFeaturesLibrary/Properties/AssemblyInfo.cs
+++ EnhancedFeaturesLibrary/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Doena Soft.")]
 [assembly: AssemblyProduct("Enhanced Features Library")]
-[assembly: AssemblyCopyright("Copyright © Doena Soft. 2017 - 2019")]
+[assembly: AssemblyCopyright("Copyright © Doena Soft. 2017 - 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 
-[assembly: AssemblyVersion("1.0.1.1")]
-[assembly: AssemblyFileVersion("1.0.1.1")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 
 [assembly: AssemblyDelaySign(false)]
